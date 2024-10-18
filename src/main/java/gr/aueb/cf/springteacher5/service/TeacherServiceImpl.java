0a80198b6e2ad03d7c4029ceb8f717e3a5f443fb
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-10-18 04:49:42 +0200

    Spring Project

--- src/main/java/gr/aueb/cf/springteacher5/service/TeacherServiceImpl.java
+++ src/main/java/gr/aueb/cf/springteacher5/service/TeacherServiceImpl.java
@@ -1,22 +1,21 @@
 package gr.aueb.cf.springteacher5.service;
 
+
 import gr.aueb.cf.springteacher5.dto.TeacherInsertDTO;
 import gr.aueb.cf.springteacher5.dto.TeacherUpdateDTO;
 import gr.aueb.cf.springteacher5.mapper.Mapper;
 import gr.aueb.cf.springteacher5.model.Teacher;
 import gr.aueb.cf.springteacher5.repository.TeacherRepository;
 import gr.aueb.cf.springteacher5.service.exceptions.EntityNotFoundException;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 
 @Service
 @Slf4j
@@ -37,28 +36,30 @@ public class TeacherServiceImpl implements ITeacherService {
 
         try {
             teacher = teacherRepository.save(Mapper.mapToTeacher(dto));
-            if (teacher.getId() == null) {
-                throw new Exception("Insert Error");
+            if(teacher.getId() == null) {
+                throw new Exception("Insert error")
             }
-            // If entity already exists DO NOT insert again
-            log.info("insert success for teacher with id: " + teacher.getId());
+            log.info("Inserted teacher with id: " + teacher.getId());
         } catch (Exception e) {
-            log.error(e.getMessage());
+            log.error("insert error: " + e.getMessage());
             throw e;
         }
         return teacher;
     }
 
-    @Transactional
     @Override
+    @Transactional
     public Teacher updateTeacher(TeacherUpdateDTO dto) throws EntityNotFoundException {
         Teacher teacher;
         Teacher updatedTeacher;
         try {
             teacher = teacherRepository.findTeacherById(dto.getId());
-            if (teacher == null) throw new EntityNotFoundException(Teacher.class, dto.getId());
+            if(teacher == null) {
+                throw new EntityNotFoundException(Teacher.class, dto.getId());
             updatedTeacher = teacherRepository.save(Mapper.mapToTeacher(dto));
-            log.info("Teacher with id: " + updatedTeacher.getId() + " was updated");
+            log.info("Updated teacher with id: " + updatedTeacher.getId());
+            }
+
         } catch (EntityNotFoundException e) {
             log.error(e.getMessage());
             throw e;
@@ -66,49 +67,18 @@ public class TeacherServiceImpl implements ITeacherService {
         return updatedTeacher;
     }
 
-    @Transactional
     @Override
     public Teacher deleteTeacher(Long id) throws EntityNotFoundException {
-        Teacher teacher = null;
-
-        try {
-            teacher = teacherRepository.findTeacherById(id);
-            if (teacher == null) throw new EntityNotFoundException(Teacher.class, id);
-            teacherRepository.deleteById(id);
-        } catch (EntityNotFoundException e) {
-            log.error(e.getMessage());
-            throw e;
-        }
-        return teacher;
+        return null;
     }
 
     @Override
     public List<Teacher> getTeachersByLastname(String lastname) throws EntityNotFoundException {
-        List<Teacher> teachers = new ArrayList<>();
-
-        try {
-            teachers = teacherRepository.findByLastnameStartingWith(lastname);
-            if (teachers.isEmpty()) throw new EntityNotFoundException(Teacher.class, 0L);
-            log.info("Teachers with lastname starting with " + lastname + " were found");
-        } catch (EntityNotFoundException e) {
-            log.error(e.getMessage());
-            throw e;
-        }
-        return teachers;
+        return List.of();
     }
 
     @Override
     public Teacher getTeacherById(Long id) throws EntityNotFoundException {
-        Teacher teacher;
-
-        try {
-            teacher = teacherRepository.findTeacherById(id);
-            if (teacher == null) throw new EntityNotFoundException(Teacher.class, id);
-            log.info("Teacher with id " + id + " was found");
-        } catch (EntityNotFoundException e) {
-            log.error(e.getMessage());
-            throw e;
-        }
-        return teacher;
+        return null;
     }
 }
